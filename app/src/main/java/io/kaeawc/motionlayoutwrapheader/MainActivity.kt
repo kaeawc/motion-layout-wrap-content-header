db769f68f21226502f19c8a60764007d5f78d0ce
Jason Pearson <jason.d.pearson@gmail.com>
2019-04-29 23:14:47 -0400

    add logging

--- app/src/main/java/io/kaeawc/motionlayoutwrapheader/MainActivity.kt
+++ app/src/main/java/io/kaeawc/motionlayoutwrapheader/MainActivity.kt
@@ -2,7 +2,9 @@ package io.kaeawc.motionlayoutwrapheader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
 import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycler_view?.adapter = WidgetAdapter()
+
+        motion_root?.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+                Timber.d("started")
+            }
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {
+                Timber.d("progress $progress")
+            }
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                Timber.d("completed")
+            }
+        })
     }
 }
